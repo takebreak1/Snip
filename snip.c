5a373bc6cb32b3cb05a25d1c1ecce39bcd1f0d9d
takebreak1 <aaaryanbajaj02@gmail.com>
2024-12-07 22:15:08 +0530

    Entering raw mode

--- snip.c
+++ snip.c
@@ -3,40 +3,62 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <stdio.h>
+#include <errno.h>
 
 
 struct termios orig_termios;
 
+void die(const char *s)
+{
+	perror(s);
+	exit(1);
+}
+
 void disableRawMode()
 {
-	tcsetattr(STDIN_FILENO,TCSAFLUSH , &orig_termios);
+	if(tcsetattr(STDIN_FILENO,TCSAFLUSH , &orig_termios) == -1) die("tcsetattr");
 }
+
+
 void enableRawMode() 
 {
-  tcgetattr(STDIN_FILENO, &orig_termios);
-  atexit(disableRawMode);
+	if(tcgetattr(STDIN_FILENO, &orig_termios)==-1) die("tcgetattr");
+	atexit(disableRawMode);
   
-  struct termios raw=orig_termios;
-  raw.c_lflag &= ~(ECHO);
-  tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw);
+	struct termios raw=orig_termios;
+	raw.c_iflag &= ~(BRKINT | ICRNL | IXON | ISTRIP | INPCK);
+	raw.c_oflag &= ~(OPOST);
+	raw.c_cflag &= ~(CS8);
+	raw.c_lflag &= ~(ECHO | ICANON | ISIG | IEXTEN);
+	raw.c_cc[VMIN] =0;
+	raw.c_cc[VTIME]=1;
+  
+  
+	if(tcsetattr(STDIN_FILENO,TCSAFLUSH,&raw)==-1) die("tcsetattr");
 }
 
 
 int main()
 {
 	enableRawMode();
-	char c;
-	while(read(STDIN_FILENO, &c,1) == 1 && c!='q')
-	{
+	
+	while(1){
+	char c = '\0';
+	if(read(STDIN_FILENO, &c,1) == -1 && errno!=EAGAIN) die("read");
+	
 		if(iscntrl(c))
 		{
-		printf("%d\n",c);
+		printf("%d\r\n",c);
 		}
-		else'
-		'
+		else
 		{
-			printf("%d ('%c')\n",c,c);
+			printf("%d ('%c')\r\n",c,c);
+		}
+		if (c=='q')
+		{
+			break;
 		}
 	}
+
 	return 0;
 }
